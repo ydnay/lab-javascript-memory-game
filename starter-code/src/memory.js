3667b4fd8c4a0334cd9514c89be8d3a69074910d
Yandy Gonzalez <yandygonzalez@gmail.com>
2018-04-21 15:45:07 -0400

    Add shuffleCard method

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -5,9 +5,18 @@ let MemoryGame = function (cards) {
   this.pairsGuessed = 0; // Jasmine error. Number, not array + name arror.
 };
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (cardsArr) {
+  myCardsArr = cardsArr;
+  shuffledArr = [];
+  while (myCardsArr.length > 0) {
+    randomCard = myCardsArr[Math.floor(Math.random() * myCardsArr.length)];
+    cardIndex = myCardsArr.indexOf(randomCard);
+    shuffledArr.push(randomCard);
+    myCardsArr.splice(cardIndex, 1);
+  }
 
-// };
+  return shuffledArr;
+};
 
 // MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
 
